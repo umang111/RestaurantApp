835c736ce3941f65f8d136d8f908c91c90058e71
umang111 <51570750+umang111@users.noreply.github.com>
2022-10-12 21:01:14 +0530

    Delete Resturant-menu-app directory

--- Resturant-menu-app/src/main/java/com/restaurant/app/repository/FoodCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurant.app.entity.FoodCategory;
-
-public interface FoodCategoryRepo extends JpaRepository<FoodCategory, Integer>{
-
-}
