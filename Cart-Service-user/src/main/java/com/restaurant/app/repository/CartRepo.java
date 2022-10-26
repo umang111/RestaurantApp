5f77355f485978b19ba6227ab11e3af102afd0ec
umang111 <51570750+umang111@users.noreply.github.com>
2022-10-26 17:23:33 +0530

    Delete Cart-Service-user directory

--- Cart-Service-user/src/main/java/com/restaurant/app/repository/CartRepo.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurant.app.entity.Cart;
-
-public interface CartRepo extends JpaRepository<Cart,Integer> {
-
-}
