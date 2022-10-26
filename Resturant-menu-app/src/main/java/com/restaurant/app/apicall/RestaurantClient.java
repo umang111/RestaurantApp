fe6662f3f34e6a384242f96afbc43cf9b17fca3d
umang111 <51570750+umang111@users.noreply.github.com>
2022-10-26 17:29:59 +0530

    Delete Resturant-menu-app directory

--- Resturant-menu-app/src/main/java/com/restaurant/app/apicall/RestaurantClient.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.app.apicall;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value="Restaurant-app", url="http://localhost:8091/restaurant/getRestaurant/{restaurantId}")
-public interface RestaurantClient {
-
-}
